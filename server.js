5190806ed165e300261bc41874a169cf00aa84af
rikaz81 <rikaz81@gmail.com>
2017-06-07 23:28:46 +0530

    heruoke setup

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+//app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src')));
 
-console.log(path.join(__dirname, 'dist'));
+//console.log(path.join(__dirname, 'dist'));
+console.log(path.join(__dirname, 'src'));
 
 // Catch all other routes and return the index file
 app.get('/*', (req, res) => {
